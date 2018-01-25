1e58caa2f7f497fb2ffb89cd28d2e652e9fdb08e
Shilin Victor <chrono.monochrome@gmail.com>
2018-01-25 11:13:43 +0300

    ext4_utils: fixes for init
    Change-Id: Ifeaf50eef220276e2fb7f9558aa9a7b6c7072738

--- su/Android.mk
+++ su/Android.mk
@@ -1,13 +1,40 @@
-LOCAL_PATH:= $(call my-dir)
+# Root AOSP source makefile
+# su is built here, and 
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS := -Wall -Werror
-
-LOCAL_SRC_FILES:= su.cpp
-
-LOCAL_MODULE:= su
-
+LOCAL_MODULE := su
+LOCAL_MODULE_TAGS := optional
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libasync_safe \
+    libbacktrace \
+    libbase \
+    libbinder \
+    libcutils \
+    liblog \
+    liblzma \
+    libunwind \
+    libutils \
+    libvndksupport
+LOCAL_SRC_FILES := su.c daemon.c utils.c pts.c
+LOCAL_SRC_FILES += binder/appops-wrapper.cpp binder/pm-wrapper.c
+LOCAL_CFLAGS += -Werror -Wall
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-LOCAL_MODULE_TAGS := debug
+
+LOCAL_INIT_RC := superuser.rc
 
 include $(BUILD_EXECUTABLE)
+
+SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,su)
+$(SYMLINKS):
+	@echo "Symlink: $@ -> /system/xbin/su"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf ../xbin/su $@
+
+# We need this so that the installed files could be picked up based on the
+# local module name
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
+    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
+
